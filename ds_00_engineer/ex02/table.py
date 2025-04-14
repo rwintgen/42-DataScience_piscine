d5bbb601b2a89add1bce5b24b74d0101b1412b82
Romain Wintgens <rwintgen@student.42madrid.com>
2025-04-14 15:16:06 +0200

    ex02 merged into ex03 and ex02 now follows subject

--- ds_00_engineer/ex02/table.py
+++ ds_00_engineer/ex02/table.py
@@ -1,66 +1,41 @@
-import os
 import psycopg2
-import pandas as pd
 
 def connect_to_db():
 	return psycopg2.connect(
 		dbname='piscineds',
 		user='rwintgen',
-		password='password',
+		password='mysecretpassword',
 		host='localhost',
 		port='5432'
 	)
 
-def find_col_types(df):
-	type_dict = {
-		'int64': 'BIGINT',
-		'float64': 'NUMERIC(10, 2)',
-		'bool': 'BOOLEAN',
-		'datetime64[ns]': 'TIMESTAMPTZ',
-		'object': 'TEXT'
-	}
-
-	column_types = {}
-	for column in df.columns:
-		dtype = str(df[column].dtype)
-		if dtype == 'object' and df[column].str.match(r'^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} UTC$').any():
-			column_types[column] = 'TIMESTAMPTZ'
-		elif dtype in type_dict:
-			column_types[column] = type_dict[dtype]
-		else:
-			column_types[column] = 'TEXT'
-	return column_types
-
-def create_table(cur, table_name, column_types):
-	columns = ', '.join(f'{col} {col_type}' for col, col_type in column_types.items())
-	query = f'CREATE TABLE IF NOT EXISTS {table_name} ({columns});'
+def create_table(cur, table_name, headers):
+	column_types = ['TIMESTAMPTZ', 'TEXT', 'INTEGER', 'NUMERIC(10, 2)', 'BIGINT', 'UUID']
+	columns = ', '.join(f"{header} {column_type}" for header, column_type in zip(headers, column_types))
+	query = f"CREATE TABLE IF NOT EXISTS {table_name} ({columns});"
 	cur.execute(query)
 
-def populate_table(cur, table_name, df):
-	placeholders = ', '.join(['%s'] * len(df.columns))
-	column_names = ', '.join(df.columns)
-	query = f'INSERT INTO {table_name} ({column_names}) VALUES ({placeholders})'
-	for row in df.itertuples(index=False, name=None):
+def populate_table(cur, table_name, headers, rows):
+	cols = ', '.join(headers)
+	placeholders = ', '.join(['%s'] * len(headers))
+	query = f"INSERT INTO {table_name} ({cols}) VALUES ({placeholders})"
+	for row in rows:
 		cur.execute(query, row)
 
-def process_csv(file_path, table_name, cur):
-	df = pd.read_csv(file_path)
-	column_types = find_col_types(df)
-	create_table(cur, table_name, column_types)
-	populate_table(cur, table_name, df)
-
 def main():
-	path_to_folder = './customer'
-	files = [file for file in os.listdir(path_to_folder) if file.endswith('.csv')]
+	file_path = './customer/data_2023_jan.csv'
+	table_name = 'data_2023_jan'
 
-	if not files:
-		return
+	with open(file_path, 'r') as f:
+		lines = [line.strip() for line in f if line.strip()]
+		headers = lines[0].split(',')
+		data_rows = [line.split(',') for line in lines[1:]]
+
 	with connect_to_db() as conn:
 		with conn.cursor() as cur:
-			for file in files:
-				file_path = os.path.join(path_to_folder, file)
-				process_csv(file_path, file.split('.')[0], cur)
-				conn.commit()
+			create_table(cur, table_name, headers)
+			populate_table(cur, table_name, headers, data_rows)
+			conn.commit()
 
 if __name__ == '__main__':
 	main()
---
+++ ds_00_engineer/ex03/automatic_table.py
@@ -0,0 +1,66 @@
+import os
+import psycopg2
+import pandas as pd
+
+def connect_to_db():
+	return psycopg2.connect(
+		dbname='piscineds',
+		user='rwintgen',
+		password='password',
+		host='localhost',
+		port='5432'
+	)
+
+def find_col_types(df):
+	type_dict = {
+		'int64': 'BIGINT',
+		'float64': 'NUMERIC(10, 2)',
+		'bool': 'BOOLEAN',
+		'datetime64[ns]': 'TIMESTAMPTZ',
+		'object': 'TEXT'
+	}
+
+	column_types = {}
+	for column in df.columns:
+		dtype = str(df[column].dtype)
+		if dtype == 'object' and df[column].str.match(r'^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} UTC$').any():
+			column_types[column] = 'TIMESTAMPTZ'
+		elif dtype in type_dict:
+			column_types[column] = type_dict[dtype]
+		else:
+			column_types[column] = 'TEXT'
+	return column_types
+
+def create_table(cur, table_name, column_types):
+	columns = ', '.join(f'{col} {col_type}' for col, col_type in column_types.items())
+	query = f'CREATE TABLE IF NOT EXISTS {table_name} ({columns});'
+	cur.execute(query)
+
+def populate_table(cur, table_name, df):
+	placeholders = ', '.join(['%s'] * len(df.columns))
+	column_names = ', '.join(df.columns)
+	query = f'INSERT INTO {table_name} ({column_names}) VALUES ({placeholders})'
+	for row in df.itertuples(index=False, name=None):
+		cur.execute(query, row)
+
+def process_csv(file_path, table_name, cur):
+	df = pd.read_csv(file_path)
+	column_types = find_col_types(df)
+	create_table(cur, table_name, column_types)
+	populate_table(cur, table_name, df)
+
+def main():
+	path_to_folder = './customer'
+	files = [file for file in os.listdir(path_to_folder) if file.endswith('.csv')]
+
+	if not files:
+		return
+	with connect_to_db() as conn:
+		with conn.cursor() as cur:
+			for file in files:
+				file_path = os.path.join(path_to_folder, file)
+				process_csv(file_path, file.split('.')[0], cur)
+				conn.commit()
+
+if __name__ == '__main__':
+	main()
