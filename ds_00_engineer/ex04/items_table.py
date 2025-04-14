41e81b7500b326a075538d366961e86e1d357a85
Romain Wintgens <rwintgen@student.42madrid.com>
2025-04-14 18:34:04 +0200

    adapted ex02 for ex04

--- ds_00_engineer/ex04/items_table.py
+++ ds_00_engineer/ex04/items_table.py
@@ -10,8 +10,8 @@ def connect_to_db():
 	)
 
 def create_table(cur, table_name, headers):
-	column_types = ['BIGINT', 'BIGINT', 'TEXT', 'TEXT']
-	columns = ', '.join(f"{header} {col_type}" for header, col_type in zip(headers, column_types))
+	column_types = ['TIMESTAMPTZ', 'TEXT', 'INTEGER', 'NUMERIC(10, 2)', 'BIGINT', 'UUID']
+	columns = ', '.join(f"{header} {column_type}" for header, column_type in zip(headers, column_types))
 	query = f"CREATE TABLE IF NOT EXISTS {table_name} ({columns});"
 	cur.execute(query)
 
@@ -20,11 +20,12 @@ def populate_table(cur, table_name, headers, rows):
 	placeholders = ', '.join(['%s'] * len(headers))
 	query = f"INSERT INTO {table_name} ({cols}) VALUES ({placeholders})"
 	for row in rows:
+		row = [value if value != '' else None for value in row]
 		cur.execute(query, row)
 
 def main():
-	file_path = './customer/data_2023_jan.csv'
-	table_name = 'data_2023_jan'
+	file_path = './items/items.csv'
+	table_name = 'items'
 
 	with open(file_path, 'r') as f:
 		lines = [line.strip() for line in f if line.strip()]
