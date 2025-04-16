b5cc6b86734e3a57b83bfd5bf2a21eb06a14b568
Romain Wintgens <rwintgen@student.42madrid.com>
2025-04-16 18:42:25 +0200

    ex01 done

---
+++ ds_01_warehouse/ex01/customers_table.py
@@ -0,0 +1,53 @@
+import os
+import psycopg2
+import pandas as pd
+
+MAX_ROWS = 10000
+
+def connect_to_db():
+	return psycopg2.connect(
+		dbname='piscineds',
+		user='rwintgen',
+		password='mysecretpassword',
+		host='localhost',
+		port='5432'
+	)
+
+def create_and_populate_table(path_to_folder, table_name, cur):
+	dataframes = []
+
+	print("Creating combined table...")
+	for file in os.listdir(path_to_folder):
+		if file.endswith('.csv'):
+			file_path = os.path.join(path_to_folder, file)
+			df = pd.read_csv(file_path, nrows=MAX_ROWS)
+			dataframes.append(df)
+	combined_df = pd.concat(dataframes, ignore_index=True)
+	combined_df = combined_df.where(pd.notnull(combined_df), None)
+
+	column_types = ['TIMESTAMPTZ', 'TEXT', 'INTEGER', 'NUMERIC(10, 2)', 'BIGINT', 'UUID']
+	columns = ', '.join(f"{header} {col_type}" for header, col_type in zip(combined_df.columns, column_types))
+	create_query = f"CREATE TABLE IF NOT EXISTS {table_name} ({columns});"
+	cur.execute(create_query)
+
+	print("Populating combined table...")
+	for _, row in combined_df.iterrows():
+		placeholders = ', '.join(['%s'] * len(row))
+		insert_query = f"INSERT INTO {table_name} VALUES ({placeholders});"
+		cur.execute(insert_query, tuple(row))
+
+def main():
+	path_to_folder = './customer'
+	table_name = 'customers'
+
+	try:
+		with connect_to_db() as conn:
+			with conn.cursor() as cur:
+				create_and_populate_table(path_to_folder, table_name, cur)
+				conn.commit()
+				print(f"Table created and populated: {table_name}")
+	except Exception as e:
+		print(f"Error: {e}")
+
+if __name__ == '__main__':
+	main()
