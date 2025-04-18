6da766d3032d3a6f98d30b3a82a6f603e961a8cf
Romain Wintgens <rwintgen@student.42madrid.com>
2025-04-18 12:06:25 +0200

    ex00 done

---
+++ ds_02_analyst/ex00/pie.py
@@ -0,0 +1,41 @@
+import psycopg2
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def connect_to_db():
+	return psycopg2.connect(
+		dbname='piscineds',
+		user='rwintgen',
+		password='mysecretpassword',
+		host='localhost',
+		port='5432'
+	)
+
+def count_event_type_values():
+	query = """
+		SELECT event_type, COUNT(*) AS count
+		FROM customers
+		GROUP BY event_type;
+	"""
+	with connect_to_db() as conn:
+		df = pd.read_sql_query(query, conn)
+	return df
+
+def draw_chart(df):
+	event_types = df['event_type']
+	count = df['count']
+
+x	plt.pie(count, labels=event_types, autopct='%1.1f%%')
+	plt.show()
+
+def main():
+	try:
+		print("Generating pie chart...")
+		df = count_event_type_values()
+		draw_chart(df)
+		print("Pie chart generated")
+	except Exception as e:
+		print(f"Error: {e}")
+
+if __name__ == '__main__':
+	main()
